bc0c44c449b3525d122e7f780789c7b2eac58d91
Oleg Goncharov <oleg.goncharov@kofax.com>
2021-04-24 01:21:56 +0300

    [add] swagger

--- src/Gonis.Assistant.Server/Startup.cs
+++ src/Gonis.Assistant.Server/Startup.cs
@@ -1,9 +1,14 @@
 ﻿using Gonis.Assistant.Telegram;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace Gonis.Assistant.Server
 {
@@ -20,6 +25,8 @@ namespace Gonis.Assistant.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            services.AddSwaggerGen();
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddTelegramBot();
@@ -36,6 +43,9 @@ namespace Gonis.Assistant.Server
             {
                 app.UseExceptionHandler("/Error");
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPItest v1"));
 
             app.UseStaticFiles();
 
